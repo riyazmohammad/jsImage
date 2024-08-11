ff4ce6899ec9cc542727e125e9cf0479fc94424e
riyazmohammad <aijazsiddique@gmail.com>
2024-08-11 21:23:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.post('/process_image', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
